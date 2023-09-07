6456ee740aa4ca8030262dea506598f0707b28b3
Vanessa <vmarieharmon@gmail.com>
2023-09-07 11:49:10 -0500

    modelform and automobile form functioning

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -9,49 +9,64 @@ root.render(
   </React.StrictMode>
 );
 
-async function loadModels() {
-  const response = await fetch("http://localhost:8100/api/models/");
-  if (response.ok) {
-    const modelData = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App models={modelData.models} />
-      </React.StrictMode>
-    );
-  } else {
-    console.error("ERROR", response);
-  }
-}
-loadModels();
+// async function loadManufacturers() {
+//   const response = await fetch("http://localhost:8100/api/manufacturers/");
+//   if (response.ok) {
+//     const manufacturerData = await response.json();
+//     root.render(
+//       <React.StrictMode>
+//         <App manufacturers={manufacturerData.manufacturers} />
+//       </React.StrictMode>
+//     );
+//   } else {
+//     console.error("ERROR", response);
+//   }
+// }
+// loadManufacturers();
+
+// async function loadModels() {
+//   const response = await fetch("http://localhost:8100/api/models/");
+//   if (response.ok) {
+//     const modelData = await response.json();
+//     root.render(
+//       <React.StrictMode>
+//         <App models={modelData.models} />
+//       </React.StrictMode>
+//     );
+//   } else {
+//     console.error("ERROR", response);
+//   }
+// }
+// loadModels();
 
 
-async function loadAutomobiles() {
-  const response = await fetch("http://localhost:8100/api/automobiles/");
-  if (response.ok) {
-    const automobileData = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App automobiles={automobileData.automobiles} />
-      </React.StrictMode>
-    );
-  } else {
-    console.error("ERROR", response);
-  }
-}
-loadAutomobiles();
+// async function loadAutomobiles() {
+//   const response = await fetch("http://localhost:8100/api/automobiles/");
+//   if (response.ok) {
+//     const automobileData = await response.json();
+//     root.render(
+//       <React.StrictMode>
+//         <App automobiles={automobileData.autos} />
+//       </React.StrictMode>
+//     );
+//   } else {
+//     console.error("ERROR", response);
+//   }
+// }
+// loadAutomobiles();
 
 
-async function loadSalespeople() {
-  const response = await fetch('http://localhost:8090/api/salespeople/');
-  if (response.ok) {
-    const salespeopleData = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App salespeople={salespeopleData.salespeople} />
-      </React.StrictMode>
-    );
-  } else {
-    console.error("ERROR", response);
-  }
-}
-loadSalespeople();
+// async function loadSalespeople() {
+//   const response = await fetch('http://localhost:8090/api/salespeople/');
+//   if (response.ok) {
+//     const salespeopleData = await response.json();
+//     root.render(
+//       <React.StrictMode>
+//         <App salespeople={salespeopleData.salespeople} />
+//       </React.StrictMode>
+//     );
+//   } else {
+//     console.error("ERROR", response);
+//   }
+// }
+// loadSalespeople();
